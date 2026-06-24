7b35bfbe8e4e534e295dde95faf71e7712d4c8e7
Egil <egil.ryengen@gmail.com>
2026-06-24 14:42:42 +0200

    chore: add module-resolver alias @ -> src and tsconfig paths

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+﻿module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src'
+          }
+        }
+      ],
+      ['@babel/plugin-proposal-class-properties', { "loose": true }],
+      ['@babel/plugin-proposal-private-methods', { "loose": true }]
+    ]
+  };
+};
