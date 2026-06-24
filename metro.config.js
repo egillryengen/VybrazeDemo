74836d37812cbb273ff1029736fcf5cbfe03b1fc
Egil <egil.ryengen@gmail.com>
2026-06-24 14:55:56 +0200

    chore(migration): add icons barrel and metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,25 @@
+﻿const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+const { transformer, resolver } = config;
+
+module.exports = {
+  ...config,
+  transformer: {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    ...resolver,
+    assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg', 'cjs'],
+    extraNodeModules: {
+      '@': path.resolve(__dirname, 'src'),
+      ...resolver.extraNodeModules
+    }
+  },
+  watchFolders: [
+    path.resolve(__dirname, 'src')
+  ],
+};
